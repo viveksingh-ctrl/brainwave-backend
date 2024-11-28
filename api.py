a2a30c97e6a055518f7d4f873c19d538d575b006
viveksingh-ctrl <87975235+viveksingh-ctrl@users.noreply.github.com>
2024-11-28 19:36:40 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -4,10 +4,10 @@ from groq import Groq
 from dotenv import load_dotenv
 import os
 from fastapi.responses import StreamingResponse
-from prompts import SUMMARIZE, BRAINSTORM
+from extras import BLOG_CONTENT_TYPE
+from prompts import ENTRY_MAPPER, GRAMMAR, SUMMARIZE, BRAINSTORM, TEMPLATE_RTE
 import asyncio
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 import json
 from typing import List, Dict
 import uuid 
@@ -15,6 +15,17 @@ from datetime import datetime
 import random
 import string
 from fastapi.middleware.cors import CORSMiddleware
+from llm import ChatGPTLLM
+
+
+DB_2 = 'TEST_CM'
+COLLECTION_NAME_2 = 'TEST'
+MONGO_URI = "mongodb://localhost:27017/"
+client = MongoClient(MONGO_URI)
+db2 = client[DB_2]
+c2 = db2[COLLECTION_NAME_2]
+
+llm = ChatGPTLLM()
 
 def generate_random_id():
     """Generate a 16-character random alphanumeric string starting with 'cs'."""
@@ -28,10 +39,15 @@ MONGO_URI = "mongodb://localhost:27017/"
 DATABASE_NAME = "test_db"
 COLLECTION_NAME = "test_collection"
 
+DB_2 = 'TEST_CM'
+COLLECTION_NAME_2 = 'TEST'
 # Initialize MongoDB Client
 client = MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
 collection = db[COLLECTION_NAME]
+
+db2 = client[DB_2]
+c2 = db2[COLLECTION_NAME_2]
 # Load environment variables from .env
 load_dotenv()
 
@@ -88,6 +104,184 @@ async def stream_groq_response(message: str, action: str = Header('summarize')):
     except Exception as e:
         yield f"Error: {str(e)}"
 
+async def async_streamer(generator):
+    try:
+        chunk_size = None  
+        random_number = str(uuid.uuid4())
+        for chunk in generator.iter_content(chunk_size=chunk_size):
+            data = chunk.decode("utf-8").split("data: ")
+            for dictionary_string in data:
+                if dictionary_string == "":
+                    continue
+                elif dictionary_string.startswith("[DONE]"):
+                    pass
+                    # response_output = {
+                    #     "id": random_number,
+                    #     "content": "[DONE]",
+                    #     "complete": True,
+                    #     "model": model,
+                    # }
+                    # yield "data: " + json.dumps(response_output) + "\n"
+                dictionary_string = dictionary_string.replace("\n", "")
+                try:
+                    json_data = json.loads(dictionary_string)
+                    if "content" in json_data["choices"][0]["delta"]:
+                        print(json_data["choices"][0]["delta"]["content"])
+                        yield json_data["choices"][0]["delta"]["content"]
+                        # response_output = {
+                        #     "id": random_number,
+                        #     "content": json_data["choices"][0]["delta"]["content"],
+                        #     "complete": False,
+                        #     "model": model,
+                        # }
+                        # yield "data: " + json.dumps(response_output) + "\n"
+                except json.JSONDecodeError as e:
+                    continue
+            await asyncio.sleep(0.1)
+    except asyncio.CancelledError as e:
+        pass
+
+INPUT_ = content_model = {
+    "created_at": "2024-05-21T06:53:54.981Z",
+    "updated_at": "2024-08-19T08:45:14.809Z",
+    "title": "Adwait",
+    "uid": "adwait",
+    "_version": 5,
+    "inbuilt_class": False,
+    "schema": [
+        {
+            "data_type": "text",
+            "display_name": "Title",
+            "field_metadata": {
+                "_default": True,
+                "version": 3
+            },
+            "mandatory": True,
+            "uid": "title",
+            "unique": True,
+            "multiple": False,
+            "non_localizable": False,
+            "min": 1,
+            "max": 50
+        },
+        {
+            "data_type": "text",
+            "display_name": "Multi Line Textbox",
+            "uid": "multi_line",
+            "field_metadata": {
+                "description": "",
+                "default_value": "",
+                "multiline": True,
+                "version": 3
+            },
+            "format": "",
+            "error_messages": {
+                "format": ""
+            },
+            "mandatory": False,
+            "multiple": False,
+            "non_localizable": False,
+            "unique": False
+        }
+    ],
+    "last_activity": {},
+    "maintain_revisions": True,
+    "description": "",
+    "DEFAULT_ACL": {
+        "others": {
+            "read": False,
+            "create": False
+        },
+        "users": [
+            {
+                "uid": "blt6bcf0337a4f6e8f7",
+                "read": True,
+                "sub_acl": {
+                    "read": True
+                }
+            }
+        ]
+    },
+    "SYS_ACL": {
+        "roles": [
+            {
+                "uid": "blt0b47d49d8e46ec0f",
+                "read": True,
+                "sub_acl": {
+                    "create": True,
+                    "read": True,
+                    "update": True,
+                    "delete": True,
+                    "publish": True
+                },
+                "update": True,
+                "delete": True
+            },
+            {
+                "uid": "blt6ad7a8acd16a5a99",
+                "read": True,
+                "sub_acl": {
+                    "create": True,
+                    "read": True,
+                    "update": True,
+                    "delete": True,
+                    "publish": True
+                }
+            },
+            {
+                "uid": "blt49c557406956107c",
+                "read": True,
+                "sub_acl": {
+                    "create": True,
+                    "read": True,
+                    "update": True,
+                    "delete": True,
+                    "publish": True
+                },
+                "update": True,
+                "delete": True
+            }
+        ],
+        "others": {
+            "read": False,
+            "create": False,
+            "update": False,
+            "delete": False,
+            "sub_acl": {
+                "read": False,
+                "create": False,
+                "update": False,
+                "delete": False,
+                "publish": False
+            }
+        }
+    },
+    "options": {
+        "is_page": False,
+        "singleton": True,
+        "sub_title": [],
+        "title": "title"
+    },
+    "abilities": {
+        "get_one_object": True,
+        "get_all_objects": True,
+        "create_object": True,
+        "update_object": True,
+        "delete_object": True,
+        "delete_all_objects": True
+    },
+    "extension_uids": []
+}
+
+class TextContent(BaseModel):
+    content: str
+
+@app.post("/generate-from-template")
+async def mapper(request: ChatRequest):
+    prompt = TEMPLATE_RTE.format(content_model = json.dumps(BLOG_CONTENT_TYPE), query = request.message)
+    print(prompt)
+    response = llm.answer(query = prompt, model = 'gpt-4o')
+    return StreamingResponse(async_streamer(response), media_type="text/plain")
 
 message_ = {
             "type": "doc",
@@ -109,19 +303,50 @@ message_ = {
         }
 # Define the FastAPI endpoint
 @app.post("/chat")
-async def chat(request: ChatRequest):
+async def chat(request: ChatRequest, action = Header('summarize')):
     message = request.message
+    if action == 'summarize':
+        prompt = SUMMARIZE.format(input_text = message)
+    elif action == 'brainstorm':
+        prompt = BRAINSTORM.format(input_text = message)
+
+    elif action == 'grammar':
+        prompt = GRAMMAR.format(input_text = message)
 
     # Check if the message is valid
     if not message:
         raise HTTPException(status_code=400, detail="Message cannot be empty")
 
     # Return a streaming response
-    return StreamingResponse(stream_groq_response(message_), media_type="text/plain")
+    return StreamingResponse(stream_groq_response(prompt), media_type="text/plain")
 
-class TextContent(BaseModel):
-    content: str
+@app.get("/templates")
+async def create_document(request: Request):
+    """Create a new document."""
+    result_ = c2.find()
+    return [serialize_document(doc) for doc in result_]
 
+@app.post("/templates")
+async def create_document(request: Request, item: TextContent):
+    """Create a new document."""
+    document = json.loads(item.content)
+    uid = generate_random_id()
+    document['uid'] = uid
+    document['last_updated'] = str(datetime.now())
+    result_ = c2.insert_one(document = document)
+    return [serialize_document(doc) for doc in result_]
+
+@app.post("/content-models")
+async def create_document(request: Request, item: TextContent):
+    """Create a new document."""
+    document = json.loads(item.content)
+    uid = generate_random_id()
+    document['uid'] = uid
+    document['last_updated'] = str(datetime.now())
+    c2.insert_one(document)
+    result_ = dict(collection.find_one({"uid": uid}))
+    result_["_id"] = str(result_["_id"])
+    return result_
 
 @app.post("/documents")
 async def create_document(request: Request, item: TextContent):
